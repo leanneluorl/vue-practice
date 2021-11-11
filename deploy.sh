754dc2d941243c8cfa7bcd500b142fc8cab51809
leanneluorl <leanne.front@gmail.com>
2021-11-11 15:08:38 +0800

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push https://github.com/leanneluorl/axios-practice.git master:gh-pages
+git push https://github.com/leanneluorl/vue-practice.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 publicPath: process.env.NODE_ENV === 'production'
-    ? '/axios-practice/'
+    ? '/vue-practice/' //repo name
     : '/'
 }
